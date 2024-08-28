80a5c95ef874b8989be1c4cc5bdc7ddef554d74b
Mustafa Ck <mustafacemkrt012@gmail.com>
2024-08-28 18:59:59 +0300

    Car sınıfı Api ve CQRS işlemleri tamalandı

--- Presentation/UdemyCarbook.WebApi/Program.cs
+++ Presentation/UdemyCarbook.WebApi/Program.cs
@@ -1,56 +1,69 @@
 using UdemyCarbook.Application.Features.CQRS.Handlers.AboutHandlers;
 using UdemyCarbook.Application.Features.CQRS.Handlers.BannerHandlers;
 using UdemyCarbook.Application.Features.CQRS.Handlers.BrandHandlers;
+using UdemyCarbook.Application.Features.CQRS.Handlers.CarHandlers;
 using UdemyCarbook.Application.Interfaces;
 using UdemyCarbook.Persistance.Context;
 using UdemyCarbook.Persistance.Repositories;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddScoped<CarBookContext>();
-builder.Services.AddScoped(typeof(IRepository<>),typeof(Repository<>)); 
-
-builder.Services.AddScoped<GetAboutQueryHandler>();
-builder.Services.AddScoped<GetAboutByIdQueryHandler>();
-builder.Services.AddScoped<CreateAboutCommandHandler>();
-builder.Services.AddScoped<UpdateAboutCommandHandler>();
-builder.Services.AddScoped<RemoveAboutCommandHandler>();
-
-
-builder.Services.AddScoped<GetBannerQueryHandler >();
-builder.Services.AddScoped<GetBannerByIdQueryHandler >();
-builder.Services.AddScoped<CreateBannerCommandHandler >();
-builder.Services.AddScoped<RemoveBannerCommandHandler >();
-builder.Services.AddScoped<UpdateBannerCommandHandler>();
-
-builder.Services.AddScoped<GetBrandQueryHandler>();
-builder.Services.AddScoped<GetBrandByIdQueryHandler>();
-builder.Services.AddScoped<CreateBrandCommandHandler>();
-builder.Services.AddScoped<UpdateBrandCommandHandler>();
-builder.Services.AddScoped<RemoveBrandCommandHandler>();
-
-
-
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddScoped<CarBookContext>();
+        builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+        builder.Services.AddScoped<GetAboutQueryHandler>();
+        builder.Services.AddScoped<GetAboutByIdQueryHandler>();
+        builder.Services.AddScoped<CreateAboutCommandHandler>();
+        builder.Services.AddScoped<UpdateAboutCommandHandler>();
+        builder.Services.AddScoped<RemoveAboutCommandHandler>();
+
+
+        builder.Services.AddScoped<GetBannerQueryHandler>();
+        builder.Services.AddScoped<GetBannerByIdQueryHandler>();
+        builder.Services.AddScoped<CreateBannerCommandHandler>();
+        builder.Services.AddScoped<RemoveBannerCommandHandler>();
+        builder.Services.AddScoped<UpdateBannerCommandHandler>();
+
+        builder.Services.AddScoped<GetBrandQueryHandler>();
+        builder.Services.AddScoped<GetBrandByIdQueryHandler>();
+        builder.Services.AddScoped<CreateBrandCommandHandler>();
+        builder.Services.AddScoped<UpdateBrandCommandHandler>();
+        builder.Services.AddScoped<RemoveBrandCommandHandler>();
+
+        builder.Services.AddScoped<GetCarQueryHandler>();
+        builder.Services.AddScoped<GetCarByIdQueryHandler>();
+        builder.Services.AddScoped<CreateCarCommandHandler>();
+        builder.Services.AddScoped<UpdateCarCommandHandler>();
+        builder.Services.AddScoped<RemoveCarCommandHandler>();
+
+
+
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
