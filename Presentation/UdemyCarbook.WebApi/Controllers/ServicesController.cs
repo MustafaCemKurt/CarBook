9d6228d891154a7f580243c2981319468778e1bc
Mustafa Ck <mustafacemkrt012@gmail.com>
2024-08-30 22:57:29 +0300

    Service sınıfı mediator ve api işlemleri tamamlandı

---
+++ Presentation/UdemyCarbook.WebApi/Controllers/ServicesController.cs
@@ -0,0 +1,83 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UdemyCarbook.Application.Features.Mediator.Commands.ServiceCommands;
+using UdemyCarbook.Application.Features.Mediator.Queries.ServiceQueries;
+
+namespace UdemyCarbook.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServicesController : ControllerBase
+    {
+
+
+        private readonly IMediator _mediator;
+
+        public ServicesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> ServiceList()
+        {
+            var values = await _mediator.Send(new GetServiceQuery());
+            return Ok(values);
+
+        }
+
+        [HttpGet("{id}")]
+
+        public async Task<IActionResult> GetService(int id)
+        {
+            var values = await _mediator.Send(new GetServiceByIdQuery(id));
+            return Ok(values);
+
+
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> CreateService(CreateServiceCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Service başarıyla eklendi");
+
+        }
+
+        [HttpDelete]
+
+        public async Task<IActionResult> RemoveService(int id)
+        {
+            await _mediator.Send(new RemoveServiceCommand(id));
+            return Ok("Service başarıyla silindi");
+
+
+        }
+
+        [HttpPut]
+
+        public async Task<IActionResult> UpdateService(UpdateServiceCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Service başarıyla güncellendi ");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
