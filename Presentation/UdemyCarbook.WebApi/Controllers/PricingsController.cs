a428f278f333b4230be60351a13b5b3aa1e75126
Mustafa Ck <mustafacemkrt012@gmail.com>
2024-08-30 18:55:25 +0300

    Pricing sınıfı mediator ve api işlemleri tamamlandı

---
+++ Presentation/UdemyCarbook.WebApi/Controllers/PricingsController.cs
@@ -0,0 +1,86 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UdemyCarbook.Application.Features.Mediator.Commands.PricingCommands;
+using UdemyCarbook.Application.Features.Mediator.Queries.PricingQueries;
+
+namespace UdemyCarbook.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PricingsController : ControllerBase
+    {
+
+
+
+        private readonly IMediator _mediator;
+
+        public PricingsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> PricingList()
+        {
+            var values = await _mediator.Send(new GetPricingQuery());
+            return Ok(values);
+
+        }
+
+        [HttpGet("{id}")]
+
+        public async Task<IActionResult> GetPricing(int id)
+        {
+            var values = await _mediator.Send(new GetPricingByIdQuery(id));
+            return Ok(values);
+
+
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> CreatePricing(CreatePricingCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Pricing başarıyla eklendi");
+
+        }
+
+        [HttpDelete]
+
+        public async Task<IActionResult> RemovePricing(int id)
+        {
+            await _mediator.Send(new RemovePricingCommand(id));
+            return Ok("Pricing başarıyla silindi");
+
+
+        }
+
+        [HttpPut]
+
+        public async Task<IActionResult> UpdatePricing(UpdatePricingCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Pricing başarıyla güncellendi ");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
