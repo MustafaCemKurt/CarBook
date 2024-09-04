20bd45dd2e7e9d7c0e9942fc6ae1313a9d1641d2
Mustafa Ck <mustafacemkrt012@gmail.com>
2024-09-04 16:28:46 +0300

    testimonial mediator tamamlandı

---
+++ Presentation/UdemyCarbook.WebApi/Controllers/TestimonialsController.cs
@@ -0,0 +1,74 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UdemyCarbook.Application.Features.Mediator.Commands.TestimonialCommands;
+using UdemyCarbook.Application.Features.Mediator.Queries.TestimonialQueries;
+
+namespace UdemyCarbook.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestimonialsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public TestimonialsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> TestimonialList()
+        {
+            var values = await _mediator.Send(new GetTestimonialQuery());
+            return Ok(values);
+
+        }
+
+        [HttpGet("{id}")]
+
+        public async Task<IActionResult> GetTestimonial(int id)
+        {
+            var values = await _mediator.Send(new GetTestimonialByIdQuery(id));
+            return Ok(values);
+
+
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> CreateTestimonial(CreateTestimonialCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Testimonial başarıyla eklendi");
+
+        }
+
+        [HttpDelete]
+
+        public async Task<IActionResult> RemoveTestimonial(int id)
+        {
+            await _mediator.Send(new RemoveTestimonialCommand(id));
+            return Ok("Testimonial başarıyla silindi");
+
+
+        }
+
+        [HttpPut]
+
+        public async Task<IActionResult> UpdateTestimonial(UpdateTestimonialCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Testimonial başarıyla güncellendi ");
+        }
+
+
+
+
+
+
+
+
+    }
+}
