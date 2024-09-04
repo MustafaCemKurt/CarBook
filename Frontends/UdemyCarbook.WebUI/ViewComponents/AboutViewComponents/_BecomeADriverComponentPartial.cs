03751196012e1bd220c28bae1434b4cc7d54a2d2
Mustafa Ck <mustafacemkrt012@gmail.com>
2024-09-04 16:04:36 +0300

    become a driver alanı

---
+++ Frontends/UdemyCarbook.WebUI/ViewComponents/AboutViewComponents/_BecomeADriverComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UdemyCarbook.WebUI.ViewComponents.AboutViewComponents
+{
+    public class _BecomeADriverComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();  
+        }
+    }
+}
