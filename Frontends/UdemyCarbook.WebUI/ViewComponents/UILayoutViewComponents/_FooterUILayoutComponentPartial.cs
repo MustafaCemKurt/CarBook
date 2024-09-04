e3d152eb0a7a5a468daeb0bfeca410e1498a9c85
Mustafa Ck <mustafacemkrt012@gmail.com>
2024-09-04 12:38:50 +0300

    view componentler tamamlandı

---
+++ Frontends/UdemyCarbook.WebUI/ViewComponents/UILayoutViewComponents/_FooterUILayoutComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UdemyCarbook.WebUI.ViewComponents.UILayoutViewComponents
+{
+    public class _FooterUILayoutComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+
+        {
+
+            return View();
+        }
+    }
+}
