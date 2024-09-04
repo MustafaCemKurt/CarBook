36b0ed644f363d30ed2bdcae477029e24be52051
Mustafa Ck <mustafacemkrt012@gmail.com>
2024-09-04 15:33:41 +0300

    json visualizer

---
+++ Frontends/UdemyCarbook.Dto/AboutDtos/ResultAboutDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyCarbook.Dto.AboutDtos
+{
+    public class ResultAboutDto
+    {
+
+            public int aboutID { get; set; }
+            public string tittle { get; set; }
+            public string description { get; set; }
+            public string imageUrl { get; set; }
+       
+
+    }
+}
